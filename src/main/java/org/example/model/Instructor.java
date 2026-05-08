4d823cb5dd0f688be29709fa7e0fb567d4f0bd4b
James Israel N Carandang <tlga.jamesisraelnario@gmail.com>
2026-05-08 15:59:24 +0800

    Fix my instructor

--- src/main/java/org/example/model/Instructor.java
+++ src/main/java/org/example/model/Instructor.java
@@ -1,31 +1,22 @@
 package org.example.model;
 
 public class Instructor extends Person {
-    int instructorID;
-    String instructorName;
-    String department;
+    private String department;
 
     public Instructor() {
 
     }
 
-    public Instructor(int instructorID, String instructorName, String department) {
-        this.instructorID = instructorID;
-        this.instructorName = instructorName;
+    public Instructor(String personID, String name, String department) {
+       super(personID, name);
+       this.department =department;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
         this.department = department;
-
-
-
-    }
-    public void display () {
-        System.out.printf("Instructor ID: %d %n" , instructorID );
-        System.out.printf("Instructor Name: %s %n" , instructorName);
-        System.out.printf("Instructor Department: %s %n" , department);
-
-    }
-
-    @Override
-    public void mainTask() {
-        System.out.println("I teach");
     }
 }
