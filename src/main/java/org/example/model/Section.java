fb35c561c38f1881b1d97d8c202ba96cb275fca3
James Israel N Carandang <tlga.jamesisraelnario@gmail.com>
2026-05-08 14:43:21 +0800

    Doing the section

--- src/main/java/org/example/model/Section.java
+++ src/main/java/org/example/model/Section.java
@@ -1,6 +1,78 @@
 package org.example.model;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Section {
+    private String sectionID;
+    private String sectionName;
+    private int maxCapacity;
+    private Instructor instructor;
+    private List <Student> enrolledStudents;
+
+    public String getSectionID() {
+        return sectionID;
+    }
+
+    public void setSectionID(String sectionID) {
+        this.sectionID = sectionID;
+    }
+
+    public String getSectionName() {
+        return sectionName;
+    }
+
+    public void setSectionName(String sectionName) {
+        this.sectionName = sectionName;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public void setEnrolledStudents(List<Student> enrolledStudents) {
+        this.enrolledStudents = enrolledStudents;
+    }
+
+    public Section (String sectionID, String sectionName, int maxCapacity) {
+        this.sectionID = sectionID;
+        this.sectionName = sectionName;
+        this.maxCapacity = maxCapacity;
+        this.enrolledStudents = new ArrayList<>();
+
+    }
+    public boolean isFull() {
+        return enrolledStudents.size() >= maxCapacity;
+    }
+    public boolean enrollStudent (Student student) {
+        if (isFull()) return false;
+        enrolledStudents.add(student);
+        return true;
+    }
+
+    public Section() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Section{id='" + sectionID + "', name='" + sectionName +
+                "', capacity=" + enrolledStudents.size() + "/" + maxCapacity + "}";
+    }
 }
